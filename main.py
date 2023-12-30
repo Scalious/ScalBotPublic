754911a4704d0e28fc34f1a5539565db6f26c490
Scal <tthisis@gmail.com>
2023-12-30 17:27:54 -0500

    moved apps apps_menu

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import discord, settings
 from discord.ext import commands
 
-from discord.ui import Button, View
-from discord import app_commands
+# from discord.ui import Button, View
+# from discord import app_commands
+import apps_menu
 
 from cogs.member_function import UserHandler
 
@@ -16,28 +17,7 @@ def run(): # Define a function to run the bot
 
     bot = commands.Bot(command_prefix=".", intents=intents)    # Create a new bot instance
 
-    # Creates a Open a Ticket apps command - commented out the target user for now
-    @bot.tree.context_menu(name = "Open a Ticket", guild = settings.GUILDS_ID)
-    @app_commands.checks.cooldown(2, 3600, key = lambda i: (i.guild_id, i.user.id)) # 2 uses per hour per user
-    async def open_ticket(interaction: discord.Interaction, message: discord.Message):
-        await interaction.response.defer()
-        admin_role = discord.utils.get(message.guild.roles, id=settings.Admin_ID.id)
-        overwrites = {
-            message.guild.default_role: discord.PermissionOverwrite(read_messages=False), # Deny everyone to see the channel
-            interaction.user: discord.PermissionOverwrite(read_messages=True), # Allow the user to see the channel
-            #message.author: discord.PermissionOverwrite(read_messages=True), # Allow the target message author to see the channel
-            bot.user: discord.PermissionOverwrite(read_messages=True), # Allow the bot to see the channel
-            admin_role: discord.PermissionOverwrite(read_messages=True) # Allow the admin role to see the channel
-        }
-        member = await message.guild.fetch_member(interaction.user.id)
-        nickname = member.nick if member.nick else member.display_name
-        channel = await message.guild.create_text_channel(name=f'ticket-{message.author.name}', overwrites=overwrites)
-        view = View()
-        view.add_item(Button(style=discord.ButtonStyle.danger, label="Close Ticket", custom_id="close_ticket"))
-        view.add_item(Button(style=discord.ButtonStyle.primary, label="Transcript", custom_id="transcript"))
-        await channel.send(f'Ticket channel created by {nickname}.', view=view)
-        await channel.send(f'Target message: {message.content}')
-        await interaction.followup.send('Ticket created.')  
+    apps_menu.setup(bot) # Setup the apps menu
   
     @bot.event # Event to run when the bot is ready
     async def on_ready(): # Define a function to run when the bot is ready
